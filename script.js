67db3f3a32a4cb240dd6a6440d3c88b015363787
Sarah <sarorra0@gmail.com>
2021-07-05 04:16:51 -0700

    add events to buttons, update scoreboard when round is played, display final winner on page when a score of 5 is reached

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-function playerInput() {
-  let input = prompt("Choose!").toLowerCase();
-  return input[0].toUpperCase() + input.slice(1);
-}
+let buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", game));
 
 function computerPlay() {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -20,25 +18,36 @@ function playRound(playerSelection, computerSelection) {
   return "computer";
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; playerScore < 5 || computerScore < 5; i++) {
-    let playerSelection = playerInput();
-    let computerSelection = computerPlay();
-    let roundResult = playRound(playerSelection, computerSelection)
-
-    if (roundResult == "tie") {
-      console.log("TIE!!!");
-    } else if (roundResult == "player") {
-      console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
-      playerScore++
-    } else {
-      console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`);
-      computerScore++
-    }
-    
-  }
-  console.log(playerScore > computerScore ? "You WIN!" : "Sorry you lost! ;(")
+function getFinalWinner(p1, p2) {
+  console.log(p1 > p2 ? "You WIN!" : "Sorry you lost! ;(");
 }
+
+function endGame() {
+    buttons.forEach(button => button.disabled=true)
+    comments.innerText = "press any key to play again! or exit"
+}
+
+function game() {
+  let playerSelection = this.innerText;
+  let computerSelection = computerPlay();
+
+  let roundResult = playRound(playerSelection, computerSelection);
+
+  // use switch
+
+  if (roundResult == "tie") {
+    console.log("TIE!!!");
+  } else if (roundResult == "player") {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`);
+    playerScore.innerText++;
+  } else {
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`);
+    computerScore.innerText++;
+  }  
+  
+  if (playerScore.innerText == 2 || computerScore.innerText == 2) {
+      endGame()
+    getFinalWinner(playerScore.innerText, computerScore.innerText);
+}
+
+  }
